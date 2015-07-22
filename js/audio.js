3afab91d1f7b844528bc56b748538d589c31a3a6
Alex Konovalov <akonovalov@at-consulting.ru>
2015-07-22 11:43:45 +0300

    add drag and drop

--- js/audio.js
+++ js/audio.js
@@ -2,6 +2,44 @@
  * Created by akonovalov on 21.07.2015.
  */
 
+$(document).ready(function () {
+    var dropZone = $('#dropZone'),
+        maxFileSize = 10000000; // максимальный размер файла - 1 мб.
+
+    // Дальше мы должны проверить поддерживает ли браузер Drag and Drop, для этого мы будем использовать FileReader функцию. Если браузер не поддерживает Drag and Drop, то внутри элемента dropZone мы напишем «Не поддерживается браузером!» и добавим класс «error».
+    if (typeof(window.FileReader) == 'undefined') {
+        dropZone.text('Не поддерживается браузером!');
+        dropZone.addClass('error');
+    }
+
+    // Следующее что мы сделаем это будет анимация эффекта перетаскивания файла на dropZone. Отслеживать эти действия мы будет с помощью событий «ondragover» и «ondragleave». Но, так как эти события не могут быть отслежены у jQuery объекта, нам необходимо обращаться не просто к «dropZone», а к «dropZone[0]».
+    dropZone[0].ondragover = function () {
+        dropZone.addClass('hover');
+        return false;
+    };
+
+    dropZone[0].ondragleave = function () {
+        dropZone.removeClass('hover');
+        return false;
+    };
+
+    // Теперь нам необходимо написать обработчик события «ondrop» — это событие когда перетянутый файл опустили. В некоторых браузерах при перетягивании файлов в окно браузера они автоматически открываются, что бы такого не произошло нам нужно отменить стандартное поведение браузера. Также нам необходимо убрать класс «hover», и добавить класс «drop».
+    dropZone[0].ondrop = function (event) {
+        event.preventDefault();
+        dropZone.removeClass('hover');
+        dropZone.addClass('drop');
+
+        // Дальше нам нужно добавить проверку на размер файла, для этого добавим в обработчик «ondrop» следующий строчки кода:
+        var file = event.dataTransfer.files[0];
+
+        if (file.size > maxFileSize) {
+            dropZone.text('Файл слишком большой!');
+            dropZone.addClass('error');
+            return false;
+        }
+    };
+});
+
 var context;
 window.addEventListener('load', init, false);
 
@@ -37,6 +75,11 @@ function playSound(buffer) {
     source.connect(context.destination);       // connect the source to the context's destination (the speakers)
     source.start(0);                           // play the source now
                                                // note: on older systems, may have to use deprecated noteOn(time);
+}
+
+function load(){
+    var source = context.createMediaElementSource(audio_player);
+    source.connect(context.destination);
 }
 
 audio_file.onchange = function(){
